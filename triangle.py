262de0f29e29282c10a0b40c565f534a85f1d28d
FarruxRavshanov <farruxlynx@gmail.com>
2022-11-30 16:10:26 +0300

    task

--- triangle.py
+++ triangle.py
@@ -14,18 +14,19 @@ class Triangle:
         Returns:
             bool: True if the triangle is valid, False otherwise
         '''
-        return (a > 0 and b > 0 and c > 0) and (a + b > c and a + c > b and b + c > a)
+        return (self.a > 0 and self.b > 0 and self.c > 0) and (self.a + self.b > self.c and self.a + self.c > self.b and self.b + self.c > self.a)
     
     def get_type(self) -> str:
         '''
         This method finds the type of the triangle.
         '''
-        if self.a == self.b or self.b == self.c or self.a == self.c:
-            return 'teng yonli'
-        if self.a == self.b and self.a == self.c:
-            return 'muntazam'
-        if self.a != self.b and self.a != self.c and self.b != self.c:
-            return 'turli tomonli'
+        if is_valid == True:
+            if self.a == self.b or self.b == self.c or self.a == self.c:
+                return 'teng yonli'
+            if self.a == self.b and self.a == self.c:
+                return 'muntazam'
+            if self.a != self.b and self.a != self.c and self.b != self.c:
+                return 'turli tomonli'
     def perimeter(self):
         '''
         This method finds the perimeter of the triangle.
@@ -35,7 +36,8 @@ class Triangle:
         Returns:
             int or float: return perimeter of the triangle if the triangle is valid, 0 otherwise
         '''
-        return self.a + self.b +self.c
+        if is_valid == True:
+            return self.a + self.b +self.c
 
     def area(self):
         '''
