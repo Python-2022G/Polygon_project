0a82163028cb71715f20d2e3eb5619d7bb91758b
azimabdurasulov <azimjon20050604@gmail.com>
2022-12-02 15:05:57 +0500

    triangle.py

--- triangle.py
+++ triangle.py
@@ -14,15 +14,31 @@ class Triangle:
         Returns:
             bool: True if the triangle is valid, False otherwise
         '''
-        return 
+        one = 0
+        if self.a > 0:
+            one += 1
+        elif self.b > 0:
+            one += 1
+        elif self.c > 0:
+            one += 1
+        elif self.a + self.b > self.c:
+            one += 1
+        elif self.a + self.c > self.b:
+            one += 1
+        elif self.b + self.c > self.a:
+            one += 1
+        return one == 6
     
     def get_type(self) -> str:
         '''
         This method finds the type of the triangle.
-        '''
+        ''' 
+        if self.is_valid() == True:
+            
         return
         
     def perimeter(self):
+    
         '''
         This method finds the perimeter of the triangle.
 
